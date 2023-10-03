dac2f1f7fa483cd59420b7a1be25dabaf9422b4e
ARedikultseva <anredikultseva@gmail.com>
2023-10-03 22:05:12 +0500

    Add project files.

---
+++ Tyuiu.RedikultsevaAA.Sprint2.Task0.V12.Lib/DataService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+// Написать программу из операций сравнений (==, !=, <, >, <=, >=, последовательность операций не должна нарушаться) и
+// арифметических выражений, которая вернет логическую последовательность(массив): (True, False, True, False, True, False),
+// при x = 1095, y = 475
+
+namespace Tyuiu.RedikultsevaAA.Sprint2.Task0.V12.Lib
+{
+    public class DataService : ISprint2Task0V12
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            bool[] res = new bool[6];
+
+            res[0] = x == y + 620;
+            res[1] = x - 620 != y;
+            res[2] = x - 621 < y;
+            res[3] = x - 621 > y;
+            res[4] = x - 620 <= y;
+            res[5] = x - 622 >= y;
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.RedikultsevaAA.Sprint2.Task0.V12.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.RedikultsevaAA.Sprint2.Task0.V12.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.RedikultsevaAA.Sprint2.Task0.V12.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b58e7b12-0423-45e4-be21-f4645130123a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
