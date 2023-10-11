6e47eebca6cc833dafd20aa4a805260825883cfb
ARedikultseva <anredikultseva@gmail.com>
2023-10-11 23:57:33 +0500

    Task4

---
+++ Tyuiu.RedikultsevaAA.Sprint2.Task4.V30.Test/DataServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.RedikultsevaAA.Sprint2.Task4.V30.Lib;
+
+namespace Tyuiu.RedikultsevaAA.Sprint2.Task4.V30.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 1;
+            double res = ds.Calculate(x, y);
+            double wait = 7;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 2;
+            double res = ds.Calculate(x, y);
+            double wait = 4;
+            Assert.AreEqual(wait, res);
+        }
+
+    }
+}
---
+++ Tyuiu.RedikultsevaAA.Sprint2.Task4.V30/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.RedikultsevaAA.Sprint2.Task4.V30.Lib;
+
+namespace Tyuiu.RedikultsevaAA.Sprint2.Task4.V30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнила: Редикульцева А.А. | АСОиУб 23-1";
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                  *");
+            Console.WriteLine("* Тема: Тернарный оператор                                                   *");
+            Console.WriteLine("* Задание #4                                                                 *");
+            Console.WriteLine("* Вариант #30                                                                *");
+            Console.WriteLine("* Выполнила: Редикульцева А.А. | АСОиУб 23-1                                 *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                   *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение с использованием  *");
+            Console.WriteLine("* тернарного оператора, гд пользователь вводит значение x, y с клавиатуры,   *");
+            Console.WriteLine("*                                  y                                         *");
+            Console.WriteLine("*                          (  4  )                3                          *");
+            Console.WriteLine("* если x*2 > y, то z = 6 + (-----)  , иначе y + -----                        *");
+            Console.WriteLine("*                          ( x*x )               x*x                         *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                           *");
+            Console.WriteLine("******************************************************************************");
+
+            Console.WriteLine("Введите значение переменной X:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение переменной Y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+            double res = ds.Calculate(x, y);
+
+
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                 *");
+            Console.WriteLine("******************************************************************************");
+
+            Console.WriteLine("Значение функции = " + res);
+
+            Console.ReadKey();
+        }
+    }
+}
