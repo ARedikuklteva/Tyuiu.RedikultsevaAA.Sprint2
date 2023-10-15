25b63dc66fda9e04f5f819757cf0e32270fceefe
ARedikultseva <anredikultseva@gmail.com>
2023-10-15 14:05:20 +0500

    Task5

---
+++ Tyuiu.RedikultsevaAA.Sprint2.Task5.V11.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.RedikultsevaAA.Sprint2.Task5.V11.Lib;
+
+namespace Tyuiu.RedikultsevaAA.Sprint2.Task5.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidFindDateOfNextDay()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual("1.1.2005", ds.FindDateOfNextDay(2004, 12, 31));
+        }
+    }
+}
---
+++ Tyuiu.RedikultsevaAA.Sprint2.Task5.V11/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.RedikultsevaAA.Sprint2.Task5.V11.Lib;
+
+namespace Tyuiu.RedikultsevaAA.Sprint2.Task5.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнила: Редикульцева А.А. | АСОиУб 23-1";
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                  *");
+            Console.WriteLine("* Тема: Оператор switch                                                      *");
+            Console.WriteLine("* Задание #5                                                                 *");
+            Console.WriteLine("* Вариант #11                                                                *");
+            Console.WriteLine("* Выполнила: Редикульцева А.А. | АСОиУб 23-1                                 *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                   *");
+            Console.WriteLine("* Написать программу, которая использует оператор switch вычисляет требуемое *");
+            Console.WriteLine("* значение и возвращает результат.                                           *");
+            Console.WriteLine("* Условие: Дата некоторого дня характеризуется тремя натуральными числами:   *");
+            Console.WriteLine("* g(год), m(порядковый номер месяца) и n(число). По заданным g, n и m        *");
+            Console.WriteLine("* определить дату следующего дня. Заданный год не является високосным.       *");
+            Console.WriteLine("*                          ( x*x )               x*x                         *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                           *");
+            Console.WriteLine("******************************************************************************");
+
+            string res;
+
+            Console.WriteLine("Введите год: ");
+            int g = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите номер месяца:");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            if (m > 12 || m < 1)
+            {
+                res = "Неверно введен номер месяца";
+
+                Console.WriteLine("Введите день:");
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                if (n > 31 || n < 1)
+                { 
+                    res = "Неверно введен номер месяца и неверно введен день"; 
+                }
+
+            }
+
+            else
+            {
+                Console.WriteLine("Введите день:");
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                if (n > 31 || n < 1)
+                {
+                    res = "Неверно введен номер месяца и неверно введен день";
+                }
+                else
+                {
+                    res = "Следующий день: " + ds.FindDateOfNextDay(g, m, n);
+                }
+            }
+
+
+
+
+                Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                 *");
+            Console.WriteLine("******************************************************************************");
+
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
