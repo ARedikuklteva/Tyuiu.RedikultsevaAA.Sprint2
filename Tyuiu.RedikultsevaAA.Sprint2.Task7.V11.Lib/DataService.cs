21a733b97679f8f25c394180cdf0c396540efb50
ARedikultseva <anredikultseva@gmail.com>
2023-10-15 20:49:39 +0500

    Task7

---
+++ Tyuiu.RedikultsevaAA.Sprint2.Task7.V11.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.RedikultsevaAA.Sprint2.Task7.V11.Lib
+{
+    public class DataService : ISprint2Task7V11
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            if (x * x + y * y <= 1)
+                if (y >= x)
+                    return true;
+                else if (x <= 0)
+                    return true;
+                else
+                    return false;
+            else
+                return false;
+        }
+    }
+}
---
+++ Tyuiu.RedikultsevaAA.Sprint2.Task7.V11.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.RedikultsevaAA.Sprint2.Task7.V11.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.RedikultsevaAA.Sprint2.Task7.V11.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("36a79c1b-257f-4aff-b8d1-fdba4aeb7466")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
